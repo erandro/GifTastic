3263a27272d0161f982d00d0eaa39a475bebd89f
erandro <erandro@gmail.com>
2018-08-14 22:20:35 -0500

    added the finished project. maybe will add more things in the future

---
+++ README.md
@@ -0,0 +1,17 @@
+# GifTastic
+
+### Overview
+This project is a webpage that show-case Gifs by topics uppon user desire.
+
+### How does it work?
+On the top of the page, there are multiple default topics. the user can choose to click on those or add more topics of his liken.
+
+when the user clicks on a topic- 10 Gif images related to this topic will appear. The user can later click on a Gif image to make it active (animated) or to stop it (by default all Gifs are displayed as still images).
+
+### Why does it useful?
+This project is an example of using an API ("Giphy" in this case) and how to draw data from it and show it in our DOM (Gifs, images, and rating in this case).
+
+In addition, this is a good example of using Javascript to not only do actions and functions- but adding content to the DOM as well.
+
+### How do the user play?
+Instructions are on the page.
---
+++ assets/javascript/javascript.js
@@ -0,0 +1,67 @@
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=ADD-YOUR-KEY-HERE";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
+
+var topics = ["basketball", "anime", "landscape architecture", "israel", "food", "coding"];
+
+function renderButtons() {
+    $("#buttonsHolder").empty();
+    topics.forEach(function (topic) {
+        var button = $("<button>").addClass("topicButton").attr("data-name", topic).text(topic);
+        $("#buttonsHolder").append(button);
+    });
+}
+renderButtons()
+
+$("#add-topic").on("click", function (event) {
+    event.preventDefault();
+    var userTopic = $("#topic-input").val().trim();
+    topics.push(userTopic);
+    renderButtons();
+});
+
+function displayGifs() {
+    var topic = $(this).attr("data-name");
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=ADD-YOUR-KEY-HERE";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        for (var i = 0; i < 10; i++) {
+            var box = $("<div>").addClass("gifBox")
+
+            var gifRating = response.data[i].rating;
+            var gifText = $("<p>").addClass("gifText").text("Rated - " + gifRating);
+            $(box).append(gifText);
+
+            var gifUrl = response.data[i].images.original.webp;
+            var stillUrl = response.data[i].images["480w_still"].url;
+            var gifImage = $("<img>").attr("src", stillUrl).addClass("gifItem").attr("data-animation", gifUrl).attr("data-still", stillUrl).attr("data-state", "still");
+            $(box).prepend(gifImage);
+
+            $("#gifHolder").prepend(box);
+        }
+    });
+}
+
+$(document).on("click", ".topicButton", displayGifs);
+
+
+$(document).on("click", ".gifItem", function (event) {
+    var gifItem = event.target
+    var state = $(gifItem).attr("data-state");
+    if (state === "still") {
+        $(gifItem).attr("src", $(gifItem).attr("data-animation"));
+        $(gifItem).attr("data-state", "animation");
+    } else {
+        $(gifItem).attr("src", $(gifItem).attr("data-still"));
+        $(gifItem).attr("data-state", "still");
+    }
+});
